4573a3a3724feac0cfacd8736808679e06bb67e3
Max <mgade@intercolo.de>
2014-07-03 23:25:43 +0200

    updated Tickets

--- app/lang/en/site.php
+++ app/lang/en/site.php
@@ -1,68 +1,74 @@
 <?php
 
 return [
-    'navi' => [
-        'home' => 'Start',
-        'profile' => 'My Profile',
-        'games' => 'Games',
-        'ips' => 'IPs',
-        'users' => 'Users',
-        'chpass' => 'Change Password',
-        'ticket' => 'Support Tickets',
-        'gameserver' => 'My Gameserver',
-        'support' => 'Help',
-        'logout' => 'Logout',
-        'scripts' => 'Scripts'
-    ],
-    'gameserver' => [
-        'connection_info' => 'Connection info',
-        'gameserver' => 'Game Server',
-        'gameservers' => 'Game Servers',
-        'status' => 'Status',
-        'action' => 'Action',
-        'start_server' => 'Start Server',
-        'stop_server' => 'Stop Server',
-        'restart_server' => 'Restart Server',
-        'config_files' => 'Config Files',
-        'install_addons' => 'install Addons/Mods',
-        'screen_log' => 'Screen Log',
-        'gameserver_stats' => 'Game Server Stats',
-        'ips' => 'IPs',
-        'ip_add' => 'Add IP',
-        'users' => 'Users',
-        'user_add' => 'Add User',
-        'games' => 'Games',
-        'game_add' => 'Add Game',
-        'gameserver_add' => 'Add Gameserver',
-        'delete_server' => 'Delete Gameserver',
-        'reinstall_server' => 'Reinstall Gameserver',
-        'change_settings' => 'Change Settings',
-        'scripts' => 'Scripts'
-    ],
-    'userprofile' => [
-        'username' => 'Username',
-        'profile' => 'My Profile',
-        'updatePassword' => 'Change Password'
-    ],
-    'user_list' => [
-        'id' => 'ID',
-        'fist_name' => 'First Name',
-        'last_name' => 'Last Name',
-        'login_name' => 'Login Name',
-        'email' => 'Email',
-        'last_login' => 'Last Login',
-        'status' => 'Status',
-        'actions' => 'Actions'
-    ],
-    'script' => [
-        'add' => 'add Script',
-        'scripts' => 'Scripts',
-        'last_name' => 'Last Name',
-        'login_name' => 'Login Name',
-        'email' => 'Email',
-        'last_login' => 'Last Login',
-        'status' => 'Status',
-        'actions' => 'Actions'
-    ]
+      'navi'        => [
+            'home'       => 'Start',
+            'profile'    => 'My Profile',
+            'games'      => 'Games',
+            'ips'        => 'IPs',
+            'users'      => 'Users',
+            'chpass'     => 'Change Password',
+            'ticket'     => 'Support Tickets',
+            'gameserver' => 'My Gameserver',
+            'support'    => 'Help',
+            'logout'     => 'Logout',
+            'scripts'    => 'Scripts'
+      ],
+      'gameserver'  => [
+            'connection_info'  => 'Connection info',
+            'gameserver'       => 'Game Server',
+            'gameservers'      => 'Game Servers',
+            'status'           => 'Status',
+            'action'           => 'Action',
+            'start_server'     => 'Start Server',
+            'stop_server'      => 'Stop Server',
+            'restart_server'   => 'Restart Server',
+            'config_files'     => 'Config Files',
+            'install_addons'   => 'install Addons/Mods',
+            'screen_log'       => 'Screen Log',
+            'gameserver_stats' => 'Game Server Stats',
+            'ips'              => 'IPs',
+            'ip_add'           => 'Add IP',
+            'users'            => 'Users',
+            'user_add'         => 'Add User',
+            'games'            => 'Games',
+            'game_add'         => 'Add Game',
+            'gameserver_add'   => 'Add Gameserver',
+            'delete_server'    => 'Delete Gameserver',
+            'reinstall_server' => 'Reinstall Gameserver',
+            'change_settings'  => 'Change Settings',
+            'scripts'          => 'Scripts'
+      ],
+      'userprofile' => [
+            'username'       => 'Username',
+            'profile'        => 'My Profile',
+            'updatePassword' => 'Change Password'
+      ],
+      'user_list'   => [
+            'id'         => 'ID',
+            'fist_name'  => 'First Name',
+            'last_name'  => 'Last Name',
+            'login_name' => 'Login Name',
+            'email'      => 'Email',
+            'last_login' => 'Last Login',
+            'status'     => 'Status',
+            'actions'    => 'Actions'
+      ],
+      'script'      => [
+            'add'        => 'add Script',
+            'scripts'    => 'Scripts',
+            'last_name'  => 'Last Name',
+            'login_name' => 'Login Name',
+            'email'      => 'Email',
+            'last_login' => 'Last Login',
+            'status'     => 'Status',
+            'actions'    => 'Actions'
+      ],
+      'ticket'      => [
+            'tickets'     => 'My Tickets',
+            'ticket'      => 'My Support Tickets',
+            'add'         => 'add new Ticket',
+            'tickets_new' => 'add new Ticket'
+      ]
 
 ];
